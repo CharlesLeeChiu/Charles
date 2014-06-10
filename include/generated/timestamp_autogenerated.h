a401d32f8cadbf11eadaa26aa240b3218098b99f
= <=>
2014-06-10 14:33:32 +0800

    u-boot_for_mpc8315

---
+++ include/generated/timestamp_autogenerated.h
@@ -0,0 +1,2 @@
+#define U_BOOT_DATE "Jun 06 2014"
+#define U_BOOT_TIME "17:04:50"
---
+++ include/generated/version_autogenerated.h
@@ -0,0 +1,4 @@
+#define PLAIN_VERSION "2012.10-gaf7e457-dirty"
+#define U_BOOT_VERSION "U-Boot 2012.10-gaf7e457-dirty"
+#define CC_VERSION_STRING "powerpc-buildroot-linux-uclibc-gcc (Buildroot 2014.05-git-00995-g8b091e1) 4.7.3"
+#define LD_VERSION_STRING "GNU ld (GNU Binutils) 2.22"
